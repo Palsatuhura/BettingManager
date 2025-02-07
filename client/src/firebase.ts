dba040af7a05c15ae4872fded62c450c2861042c
Palsatuhura <atuhuralivingstone@gmail.com>
2025-02-07 13:01:38 +0300

    Initial commit

--- client/src/firebase.ts
+++ client/src/firebase.ts
@@ -2,7 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {};
+const firebaseConfig = {
+  apiKey: "AIzaSyDvkLJqrTgJ_4PzurCxxJwI59ihsY41yr4",
+  authDomain: "bettingmanager-6530f.firebaseapp.com",
+  projectId: "bettingmanager-6530f",
+  storageBucket: "bettingmanager-6530f.firebasestorage.app",
+  messagingSenderId: "267112727408",
+  appId: "1:267112727408:web:41d3d27718c4951cae05b5",
+  measurementId: "G-7881RE6KX2",
+};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
---
+++ client/src/hooks/useAuth.ts
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+import { auth } from "../firebase";
+import { User } from "firebase/auth";
+
+export const useAuth = () => {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+    return unsubscribe;
+  });
+  return { user };
+};
